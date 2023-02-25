88b04e0320f338d1c3347476f9c901384fc9f7ce
VeloRus <ruslan-fedotov@inbox.ru>
2023-02-25 23:05:05 +0300

    task ready

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  if (str.length % 2 !== 0) {
+    return false;
+  }
+
+  let strArr = str.split('');
+  let arr = [];  
+ 
+  for (let i = 0; i < strArr.length; i++) {
+    for (let j = 0; j < bracketsConfig.length; j++) {
+       if (arr[arr.length - 1] === bracketsConfig[j][0] && strArr[i] === bracketsConfig[j][1]) {
+        arr.pop();
+      } else if (strArr[i] === bracketsConfig[j][0]) {
+        arr.push(strArr[i]);
+      }
+    }
+  }
+  if (arr.length === 0) {
+    return true;
+  } else return false;
 }
+
